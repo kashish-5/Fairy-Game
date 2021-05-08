a0d25b01875d42d8169a53ff42ecb8e2d0bb43b7
kashish-5 <76552203+kashish-5@users.noreply.github.com>
2021-05-08 07:32:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("star.png");
+	fairyImg = loadAnimation("fairyImage1.png","fairyImage2.png");
+	bgImg = loadImage("starNight.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	// fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+  
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+
+
+
+  // keyPressed();
+  drawSprites();
+
+}
+
+
+function keyPressed(){
+ //write code here
+ star.x=starBody.position.x;
+ star.y=starBody.position.y;
+
+  if(keyCode===40){
+     star.velocityY=5;
+  }
+  if(keyCode===39){
+	 fairy.x=fairy.x+10;
+    }
+   if(keyCode===37){
+	   fairy.x=fairy.x-10;
+   }
+}
